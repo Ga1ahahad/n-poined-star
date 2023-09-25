15bd091c8e36fe8b3096f65dcaf2ea67c7653f2f
galahahad <shlychkov.na@edu.spbstu.ru>
2023-09-25 12:20:43 +0300

    aaaaa

--- main.py
+++ main.py
@@ -21,14 +21,25 @@ class Triangle:
         self.p1 = points[0]
         self.p2 = points[1]
         self.p3 = points[2]
+        self.center = self.get_com()
 
     def get_com(self):
-        cmx = (self.p3.x - self.p1.x)/2
-        cmy = self.p2.y/3
-        self.com = Point(cmx,cmy)
+        cmx = (self.p3.x + self.p1.x)/2
+        cmy = self.p1.x + (self.p2.y-self.p1.y)/3
+        return Point(cmx,cmy)
+
+    def rotate(self, angle: float):
+        self.p1.rotate(self.center, angle)
+        self.p2.rotate(self.center, angle)
+        self.p3.rotate(self.center, angle)
 
 
 if __name__ == '__main__':
-    p = Point(0, -5)
-    p.rotate(Point(0, 0), m.pi/2)
-    p.print()
+    p1 = Point(-1,-1)
+    p2 = Point(0, 2)
+    p3 = Point(1, -1)
+    pts = [p1,p2,p3]
+    tri = Triangle(pts)
+    tri.center.print()
+    tri.rotate(m.pi/2)
+    tri.p2.print()
